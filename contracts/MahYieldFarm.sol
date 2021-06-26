f789938a16b1090b4439eed595800e6cf6e42611
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-06-26 02:33:01 -0400

    added some more functions in the MahYieldFarm contract and also some tests for it

--- contracts/MahYieldFarm.sol
+++ contracts/MahYieldFarm.sol
@@ -9,6 +9,12 @@ contract MahYieldFarm {
     struct StakerInfo {
         address stakerAddress;
         uint256 stakedTokens;
+        //uint256 mahTokens;
+    }
+
+    struct EarnerInfo {
+        address earnerAddress;
+        uint256 earnedTokens;
     }
 
     DaToken public daTokenContract;
@@ -18,13 +24,19 @@ contract MahYieldFarm {
 
     address payable admin;
 
-    mapping (address => StakerInfo) public stakers;
+    mapping (address => StakerInfo) public stakerInfos;
+    mapping (address => EarnerInfo) public earnerInfos;
+    address[] stakers;
+    address[] earners;
 
     event Stake(address _staker, uint256 _daTokenAmount);
+    event WithdrawStakedToken(address _staker, uint256 _withdrawTokenAmount);
+    event WithdrawEarnedToken(address _earner, uint256 _withdrawTokenAmount);
 
     constructor(DaToken _daTokenContract, MahToken _mahTokenContract) public {
         daTokenContract = _daTokenContract;
         mahTokenContract = _mahTokenContract;
+        admin = msg.sender;
     }
 
     function stakeDaToken(uint256 _daTokenAmount) public {
@@ -34,18 +46,115 @@ contract MahYieldFarm {
         daTokenContract.transferFrom(msg.sender, address(this), _daTokenAmount);
 
         if (!doesStakerExist(msg.sender)) {
-            stakers[msg.sender].stakerAddress = msg.sender;
-            stakers[msg.sender].stakedTokens = stakers[msg.sender].stakedTokens + _daTokenAmount;
+            stakerInfos[msg.sender].stakerAddress = msg.sender;
+            stakerInfos[msg.sender].stakedTokens = stakerInfos[msg.sender].stakedTokens + _daTokenAmount;
+            stakers.push(msg.sender);
         }
+
+        addStakerToEarners(msg.sender);
 
         emit Stake(msg.sender, _daTokenAmount);
     }
 
-    function doesStakerExist(address _staker) public returns (bool) {
-        return stakers[msg.sender].stakerAddress != address(0);
+    function addStakerToEarners(address _earner) private {
+        if (!doesEarnerExist(_earner)) {
+            // Initially adding earner info to the earner mapping once they stake a token
+            earnerInfos[_earner].earnerAddress = _earner;
+            earnerInfos[_earner].earnedTokens = 0;
+            earners.push(_earner);
+        }
     }
 
-    function distributeMahTokens() public {
+    function doesStakerExist(address _staker) public returns (bool) {
+        bool stakerExist = false;
 
+        for (uint i; i< stakers.length;i++){
+          if (stakers[i] == _staker)
+          stakerExist = true;
+        }
+
+        return stakerInfos[_staker].stakerAddress != address(0) && stakerExist;
+    }
+
+    function stakedTokensOfStaker(address _staker) public returns (uint256) {
+        return stakerInfos[_staker].stakedTokens;
+    }
+
+    function earnedTokensOfEarner(address _earner) public returns (uint256) {
+        return earnerInfos[_earner].earnedTokens;
+    }
+
+    function withdrawStakedTokens(uint256 _withdrawTokenAmount) public {
+        require(doesStakerExist(msg.sender));
+        require(_withdrawTokenAmount > 0);
+        require(stakerInfos[msg.sender].stakedTokens > 0); // make sure that Staker has a staked tokens
+        require(stakerInfos[msg.sender].stakedTokens >= _withdrawTokenAmount);
+        
+        stakerInfos[msg.sender].stakedTokens = stakerInfos[msg.sender].stakedTokens - _withdrawTokenAmount;
+        daTokenContract.transfer(msg.sender, _withdrawTokenAmount);
+
+        removeStaker(msg.sender);
+
+        emit WithdrawStakedToken(msg.sender, _withdrawTokenAmount);
+    }
+
+    function removeStaker(address _staker) private {
+        if (stakerInfos[_staker].stakedTokens == 0) {
+            delete stakerInfos[_staker];
+        }
+        for (uint i; i< stakers.length;i++){
+          if (stakers[i] == _staker) {
+              delete stakers[i];
+          }
+        }
+    }
+
+    function withdrawEarnedTokens(uint256 _withdrawTokenAmount) public {
+        require(doesEarnerExist(msg.sender));
+        require(_withdrawTokenAmount > 0);
+        require(earnedTokensOfEarner(msg.sender) > 0); // Make sure that earner has earned tokens
+        require(_withdrawTokenAmount <= earnedTokensOfEarner(msg.sender));
+        
+        earnerInfos[msg.sender].earnedTokens = earnerInfos[msg.sender].earnedTokens - _withdrawTokenAmount;
+        mahTokenContract.transfer(msg.sender, _withdrawTokenAmount);
+
+        removeEarner(msg.sender);
+
+        emit WithdrawEarnedToken(msg.sender, _withdrawTokenAmount);
+    }
+
+    function doesEarnerExist(address _earner) public returns (bool) {
+        bool earnerExist = false;
+
+        for (uint i; i< earners.length;i++){
+          if (earners[i] == _earner)
+          earnerExist = true;
+        }
+
+        return earnerInfos[_earner].earnerAddress != address(0) && earnerExist;
+    }
+
+    function removeEarner(address _earner) private {
+        if (earnerInfos[_earner].earnedTokens == 0) {
+            delete earnerInfos[_earner];
+        }
+        for (uint i; i< earners.length;i++){
+          if (earners[i] == _earner) {
+              delete earners[i];
+          }
+        }
+    }
+    
+    function distributeMahTokens(uint amountPerStakers) public {
+        require(msg.sender == admin);
+
+        for (uint256 i=0; i < stakers.length; i++) {
+            /*
+            if (!doesEarnerExist(stakers[i])) {
+                earnerInfos[stakers[i]].earnerAddress = stakers[i];
+                earners.push(stakers[i]);
+            }*/
+            earnerInfos[stakers[i]].earnedTokens = earnerInfos[stakers[i]].earnedTokens + amountPerStakers;
+        }
     }
 }
--- test/MahYieldFarmTest.js
+++ test/MahYieldFarmTest.js
@@ -9,6 +9,9 @@ contract('MahYieldFarm', function(accounts) {
   var daTokenInstance;
   var mahTokenInstance;
   var yieldFarmUser = accounts[1];
+  var yieldFarmUser2 = accounts[2];
+  var yieldFarmUser3 = accounts[3];
+  var admin = accounts[0];
 
   // Vars for the endTokenSaleTest
 
@@ -67,6 +70,126 @@ contract('MahYieldFarm', function(accounts) {
         return yieldFarmInstance.doesStakerExist.call(yieldFarmUser, { from: yieldFarmUser });
     }).then(function(stakerExist) {
         assert.equal(true, stakerExist, 'YieldFarmUser must be added to staker list of the YieldFarm');
+        return yieldFarmInstance.stakedTokensOfStaker.call(yieldFarmUser, { from: yieldFarmUser });
+    }).then(function(stakerTokens) {
+      assert.equal(10, stakerTokens, 'YieldFarm must keep track the staked tokens of the staker');
     });
+  });
+
+  it('Distribution of MahTokens to Stakers work as expected', function() {
+    return MahYieldFarm.deployed().then(function(instance) {
+      yieldFarmInstance = instance;
+      return DaToken.deployed();
+    }).then(function(instance) {
+      daTokenInstance = instance;
+      return MahToken.deployed();
+    }).then(function(stakerExist) {
+      // Transferring some tokens to the user
+      return daTokenInstance.transfer(yieldFarmUser, 100, { from: accounts[0] });
+    }).then(function(instance) {
+        mahTokenInstance = instance;
+        return daTokenInstance.approve(yieldFarmInstance.address, 10, { from: yieldFarmUser });
+    }).then(function(blah) {
+      var daTokenToStakeAmount = 10;
+      return yieldFarmInstance.stakeDaToken(daTokenToStakeAmount, { from: yieldFarmUser });
+    }).then(function(blah) {
+      return yieldFarmInstance.earnedTokensOfEarner.call(yieldFarmUser, { from: yieldFarmUser });
+    }).then(function(balance) {
+      assert.equal(0, balance, 'YieldFarmUser should not have any MahTokens balance in the YieldFarm');
+      var mahTokensToDistribute = 1;
+      return yieldFarmInstance.distributeMahTokens(mahTokensToDistribute, { from: yieldFarmUser });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Non-Admins cant distribute MahTokens to Stakers');
+      return yieldFarmInstance.earnedTokensOfEarner.call(yieldFarmUser, { from: yieldFarmUser });
+    }).then(function(balance) {
+      assert.equal(0, balance, 'YieldFarmUser should not have any MahTokens balance in the YieldFarm');
+      var mahTokensToDistribute = 1;
+      return yieldFarmInstance.distributeMahTokens(mahTokensToDistribute, { from: admin });
+    }).then(function(balance) {
+      return yieldFarmInstance.earnedTokensOfEarner.call(yieldFarmUser, { from: yieldFarmUser });
+    }).then(function(balance) {
+      assert.equal(1, balance, 'YieldFarmUser should not have any MahTokens balance in the YieldFarm');
+    });
+  });
+
+  it('Withdraw DaTokens should work as expected', function() {
+    return MahYieldFarm.deployed().then(function(instance) {
+      yieldFarmInstance = instance;
+      return DaToken.deployed();
+    }).then(function(instance) {
+      daTokenInstance = instance;
+      return MahToken.deployed();
+    }).then(function(stakerExist) {
+      return yieldFarmInstance.withdrawStakedTokens(10, { from: yieldFarmUser2 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Users cannot withdraw staked tokens if they havent staked yet');
+      return daTokenInstance.transfer(yieldFarmUser2, 100, { from: accounts[0] });
+    }).then(function(instance) {
+        return daTokenInstance.approve(yieldFarmInstance.address, 10, { from: yieldFarmUser2 });
+    }).then(function(blah) {
+      var daTokenToStakeAmount = 10;
+      return yieldFarmInstance.stakeDaToken(daTokenToStakeAmount, { from: yieldFarmUser2 });
+    }).then(function(stakerExist) {
+      return yieldFarmInstance.stakedTokensOfStaker.call(yieldFarmUser2, { from: yieldFarmUser2 });
+    }).then(function(stakerTokens) {
+      assert.equal(10, stakerTokens, 'YieldFarm must keep track the staked tokens of the staker');
+      return yieldFarmInstance.withdrawStakedTokens(0, { from: yieldFarmUser2 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Users cannot withdraw zero amount of staked Tokens');
+      return yieldFarmInstance.withdrawStakedTokens(10, { from: yieldFarmUser2 });
+    }).then(function(withdrawStakeEvent) {
+      assert.equal(withdrawStakeEvent.logs.length, 1, 'triggers one WithdrawStakedToken event');
+      assert.equal(withdrawStakeEvent.logs[0].event, 'WithdrawStakedToken', 'should be the "WithdrawStakedToken" event');
+      assert.equal(withdrawStakeEvent.logs[0].args._staker, yieldFarmUser2, 'should be the address of the yield farm user');
+      assert.equal(withdrawStakeEvent.logs[0].args._withdrawTokenAmount, 10, 'should have the correct withdrawn token amount');
+      return yieldFarmInstance.stakedTokensOfStaker.call(yieldFarmUser2, { from: yieldFarmUser2 });
+    }).then(function(stakerTokens) {
+      assert.equal(0, stakerTokens, 'Yield farm must reflect amount of staked tokens left for the staker');
+      return yieldFarmInstance.doesStakerExist.call(yieldFarmUser2, { from: yieldFarmUser2 });
+    }).then(function(stakerExist) {
+      assert.equal(false, stakerExist, 'YieldFarmUser must be removed from staker list of the YieldFarm');
+    });
+  });
+
+  it('Withdraw MahTokens should work as expected', function() {
+    return MahYieldFarm.deployed().then(function(instance) {
+      yieldFarmInstance = instance;
+      return DaToken.deployed();
+    }).then(function(instance) {
+      daTokenInstance = instance;
+      return MahToken.deployed();
+    }).then(function(instance) {
+        return daTokenInstance.approve(yieldFarmInstance.address, 10, { from: yieldFarmUser3 });
+    }).then(function(blah) {
+      var daTokenToStakeAmount = 10;
+      return yieldFarmInstance.stakeDaToken(daTokenToStakeAmount, { from: yieldFarmUser3 });
+    }).then(function(blah) {
+      return yieldFarmInstance.withdrawEarnedTokens(10, { from: yieldFarmUser3 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Users cannot withdraw earned tokens if they havent staked yet');
+      var mahTokensToDistribute = 1;
+      return yieldFarmInstance.distributeMahTokens(mahTokensToDistribute, { from: admin });
+    }).then(function(blah) {
+      return yieldFarmInstance.earnedTokensOfEarner.call(yieldFarmUser3, { from: yieldFarmUser3 });
+    }).then(function(earnedTokens) {
+      console.log("Earned tokens");
+      console.log(earnedTokens);
+      assert.equal(1, earnedTokens, 'YieldFarm must keep track the earned tokens of the staker');
+      return yieldFarmInstance.withdrawEarnedTokens(0, { from: yieldFarmUser3 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Users cannot withdraw earned tokens if the withdrawn amount is 0');
+      console.log("Earned tokens2");
+      //return yieldFarmInstance.withdrawEarnedTokens(2, { from: yieldFarmUser3 });
+    })/*.then(assert.fail).catch(function(error) {
+      //assert(error.message.indexOf('revert') >= 0, 'Users cannot withdraw earned tokens if the withdrawn amount is greater than what they earned');
+      console.log("Earned tokens3");
+    //  return yieldFarmInstance.withdrawEarnedTokens(1, { from: yieldFarmUser3 });
+    })*//*.then(function(withdrawEarnedEvent) {
+      assert.equal(withdrawEarnedEvent.logs.length, 1, 'triggers one WithdrawEarnedEvent event');
+      assert.equal(withdrawEarnedEvent.logs[0].event, 'WithdrawEarnedEvent', 'should be the "WithdrawEarnedEvent" event');
+      assert.equal(withdrawEarnedEvent.logs[0].args._earner, yieldFarmUser3, 'should be the address of the yield farm user');
+      assert.equal(withdrawEarnedEvent.logs[0].args._withdrawTokenAmount, 1, 'should have the correct withdrawn token amount');
+      //return yieldFarmInstance.earnedTokensOfEarner.call(yieldFarmUser2, { from: yieldFarmUser2 });
+    })*/;
   });
 });
